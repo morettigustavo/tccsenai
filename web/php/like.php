d7487910cbb712af03f85edade48586ef64a0c0e
Gustavo Moretti <gustavomoretti165@gmail.com>
2019-10-08 15:40:07 -0300

    Cadastro de likes no banco de dados

--- web/php/like.php
+++ web/php/like.php
@@ -2,13 +2,31 @@
 session_start();
 require_once('conexao.php');
 
-$titulo = $_POST['titulo'];
-$area = $_POST['area'];
-$materia = $_POST['materia'];
-$tags = $_POST['tags'];
+$id_estudante = $_GET['id_estudante'];
+$id_publicacao = $_GET['id_publicacao'];
+$tipo = $_GET['tipo'];
 
-$id_user = $_SESSION['id_estudante'];
+// SQL
+$where = "WHERE id_estudante = $id_estudante AND id_postagem = $id_publicacao";
 
-$sql = "insert into postagem (id_estudante,id_materia,titulo) values ($id_user,1,'$titulo')";
+$query = "SELECT tipo FROM curtidas $where ";
 
-mysqli_query($link, $sql);
+$sql = mysqli_query($link, $query);
+$line = mysqli_fetch_array($sql);
+
+if($line == null){
+    $query = "INSERT INTO curtidas(id_postagem, id_estudante, tipo) VALUES ($id_publicacao, $id_estudante, $tipo)";
+}else{
+    if($line['tipo'] == $tipo){
+        $query = "DELETE FROM curtidas $where";
+    }else{
+        if($line['tipo'] == 1){
+            $query = "UPDATE curtidas SET tipo = -1 $where";
+        }else{
+            $query = "UPDATE curtidas SET tipo =  1 $where";
+        }
+    }
+}
+
+
+$sql = mysqli_query($link, $query);
